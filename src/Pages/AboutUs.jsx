f4b197ed20279d46be035d128756fcdab977e1f6
nothing36 <prodipsarker953@gmail.com>
2025-11-30 18:44:30 +0600

    an update

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,6 +4,7 @@ const AboutUs = () => {
     return (
         <div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus, dolorum illum reiciendis temporibus vitae ullam perferendis quam molestiae quis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quisquam quidem atque laudantium et facere accusamus ea tempore, iure ipsa?</p>
         </div>
     );
 };
